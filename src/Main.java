172174c91a615891234e26a21377575e7addc8db
Timur Kiselev <snygens@mail.ru>
2020-06-13 20:40:47 +0300

    add project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+       Index service = new Index();
+        float BMI = 0;
+        System.out.print ("Индекс массы тела составляет: \n" + service.calculate (BMI));
+    }
+}
+// в следующий раз буду согласно требованиям делать. очень много времени убил) нужно больше опыта)
