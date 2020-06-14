dba86b77b820832f0586225c763f313628bdf51c
Timur Kiselev <snygens@mail.ru>
2020-06-14 13:47:16 +0300

    optimization

--- src/Index.java
+++ src/Index.java
@@ -1,15 +1,8 @@
 import java.util.Scanner;
 
 public class Index {
-    public float calculate (float BodyMI)
+    public float calculate (float BodyMI, float weight, float height)
     {
-        float weight = 0;
-        float height = 0;
-        Scanner in = new Scanner(System.in);
-        System.out.print ("Введите Вес \n");
-        weight = in.nextFloat();
-        System.out.print ("Введите Рост \n");
-        height = in.nextFloat();
         BodyMI = weight / (height * height) * 10000;
         return BodyMI;
     }
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,15 @@ public class Main {
     public static void main(String[] args)
     {
        Index service = new Index();
+        float weight = 0;
+        float height = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print ("Введите Вес \n");
+        weight = in.nextFloat();
+        System.out.print ("Введите Рост \n");
+        height = in.nextFloat();
         float BMI = 0;
-        System.out.print ("Индекс массы тела составляет: \n" + service.calculate (BMI));
+        System.out.print ("Индекс массы тела составляет: \n" + service.calculate (BMI, weight, height));
     }
 }
 // в следующий раз буду согласно требованиям делать. очень много времени убил) нужно больше опыта)
