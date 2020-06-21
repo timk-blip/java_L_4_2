e8f22d01aaab6a9616522606f067af026d22de01
Timur Kiselev <snygens@mail.ru>
2020-06-21 14:04:46 +0300

    отфарматирован код

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args)
-    {
-       Index service = new Index();
+    public static void main(String[] args) {
+        Index service = new Index();
         float weight = 0;
         float height = 0;
         Scanner in = new Scanner(System.in);
-        System.out.print ("Введите Вес \n");
+        System.out.print("Введите Вес \n");
         weight = in.nextFloat();
-        System.out.print ("Введите Рост \n");
+        System.out.print("Введите Рост \n");
         height = in.nextFloat();
-        System.out.print ("Индекс массы тела составляет: \n" + service.calculate (weight, height));
+        System.out.print("Индекс массы тела составляет: \n" + service.calculate(weight, height));
     }
 }
 // в следующий раз буду согласно требованиям делать. очень много времени убил) нужно больше опыта)
